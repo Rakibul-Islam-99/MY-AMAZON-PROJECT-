4f3d1021b2018b3eb9530b8d825df13143d57614
Rakibul_Islam <rakibulislamcse99@gmail.com>
2024-07-16 20:57:04 +0600

    complete the check out page by terminal

--- JScript/checkout/orderSummary.js
+++ JScript/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions,getDeliveryOption } from '../../data/deliveryOptions.js';
+import { renderPaymentsummary } from './paymentSummary.js';
 
 export function renderOrderSummary(){
 
@@ -110,7 +111,7 @@ export function renderOrderSummary(){
 
   const productId=link.dataset.productId;
   removeFromCart(productId);
-
+  renderPaymentsummary();
 
   const container=document.querySelector(`.js-cart-item-container-${productId}`);
   container.remove();
@@ -122,6 +123,7 @@ export function renderOrderSummary(){
       const {productId,deliveryOptionId}=element.dataset;
     updateDeliveryOption(productId, deliveryOptionId);
     renderOrderSummary();
+    renderPaymentsummary();
     });
   });
 }
